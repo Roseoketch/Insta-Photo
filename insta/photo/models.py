cd197a818ccf7c8872ae1af83b6146250035e633
Roseoketch <oketchrose@gmail.com>
2018-05-14 11:31:06 +0300

    models midified

--- insta/photo/models.py
+++ insta/photo/models.py
@@ -1,9 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
-from models import Profile
-from . import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True,blank=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def find_profile(cls,search_term):
+        profile = cls.objects.filter(user__username__icontains=search_term)
+        return profile
+
+    @classmethod
+    def update_profile(cls,id,bio):
+        updated = Image.objects.filter(id=id).update(bio = bio)
+        return updated
+
+
+
 class Image(models.Model):
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     caption = models.CharField(max_length = 60)
@@ -37,3 +67,26 @@ class Image(models.Model):
     def get_image_by_id(cls,id):
         image = Image.objects.filter(id=Image.id)
         return image
+
+class Comment(models.Model):
+    comments = models.CharField(max_length=60,blank=True,null=True)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comments
+
+    class Meta:
+        ordering = ['-comment_date']
+
+    def save_comment(self):
+        return self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comment(cls):
+        comment = Comment.objects.all()
+        return comment
